43bbd8bb88322fa995aa65328e9c23eeedc9343d
Leo Jonsson <leojon00003@stud.noroff.no>
2025-05-25 12:55:34 +0200

    :wip:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,9 @@ import Section from "./components/common/Section";
 import Typography from "./components/common/Typography";
 
 export default function Home() {
-  const [allUploadedUrls, setAllUploadedUrls] = useState<string[]>([]);
-
-  const handleUrlsUpdate = (urls: string[]) => {
-    console.log("All uploaded URLs:", urls);
-    setAllUploadedUrls(urls);
-  };
-
-  console.log(allUploadedUrls, "hej");
-
   return (
     <Section className="min-h-screen">
       <Typography.H1 label="Welcome" />
-      <FileUploader onUrlsChange={handleUrlsUpdate} endpoint="imageUploader" />
-
-      {allUploadedUrls?.map((url) => (
-        <div key={Math.random()}>{url}</div>
-      ))}
     </Section>
   );
 }
